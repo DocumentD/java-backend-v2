57ad7f5b80cf8da5a78ac34385d2f473db5cd1ab
Sebastian Espei <seblsebastian@aol.de>
2020-09-23 01:24:03 +0200

    Create indexes and first user on fresh db

--- src/main/java/de/skillkiller/documentdbackend/ReadyListener.java
+++ src/main/java/de/skillkiller/documentdbackend/ReadyListener.java
@@ -2,11 +2,13 @@ package de.skillkiller.documentdbackend;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.skillkiller.documentdbackend.entity.Document;
 import de.skillkiller.documentdbackend.entity.User;
 import de.skillkiller.documentdbackend.search.MeliSearch;
 import kong.unirest.Unirest;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,11 +22,19 @@ public class ReadyListener {
     private final MeliSearch meliSearch;
     private final ObjectMapper objectMapper;
     private final PasswordEncoder passwordEncoder;
+    private static final Logger logger = LoggerFactory.getLogger(ReadyListener.class);
 
-    public ReadyListener(MeliSearch meliSearch, ObjectMapper objectMapper, PasswordEncoder passwordEncoder) {
+    private final String firstUserUsername;
+    private final String firstUserPassword;
+
+    public ReadyListener(MeliSearch meliSearch, ObjectMapper objectMapper, PasswordEncoder passwordEncoder,
+                         @Value("${firstuser.username:admin}") String firstUserUsername,
+                         @Value("${firstuser.password:${random.value}}") String firstUserPassword) {
         this.meliSearch = meliSearch;
         this.objectMapper = objectMapper;
         this.passwordEncoder = passwordEncoder;
+        this.firstUserUsername = firstUserUsername;
+        this.firstUserPassword = firstUserPassword;
     }
 
     @EventListener(ApplicationReadyEvent.class)
@@ -49,30 +59,26 @@ public class ReadyListener {
                 }
             }
         });
-        System.out.println(meliSearch.getAllIndexes());
-        System.out.println("meliSearch.createUserIndex() = " + meliSearch.createUserIndex());
-        System.out.println("meliSearch.createDocumentIndex() = " + meliSearch.createDocumentIndex());
-        User user = new User();
 
-        user.setUsername("Skillkiller");
-        user.setId(DigestUtils.sha1Hex(System.currentTimeMillis() + "#" + user.getUsername()));
-        user.setModifyDate(new Date());
-        user.setPasswordHash(passwordEncoder.encode("HASH"));
-        System.out.println("user = " + user);
+        logger.trace("Create user index if not exists: " + meliSearch.createUserIndex());
+        logger.trace("Create document index if not exists: " + meliSearch.createDocumentIndex());
 
-        Document document = new Document();
+        if (!meliSearch.hasSystemUsers()) {
+            User user = new User();
 
-        document.setFilename("test.pdf");
-        document.setPages(1);
-        document.setTitle("Title");
-        document.setUserId("c714c2416ff5a0fc18a131aaf38e93a56a0f64a6");
-        document.setDocumentDate(new Date());
-        document.setId("doc3");
+            user.setUsername(firstUserUsername);
+            user.setId(DigestUtils.sha1Hex(System.currentTimeMillis() + "#" + user.getUsername()));
+            user.setModifyDate(new Date());
+            user.setPasswordHash(passwordEncoder.encode(firstUserPassword));
 
-        System.out.println("meliSearch.createOrReplaceDocument(document) = " + meliSearch.createOrReplaceDocument(document));
+            if (meliSearch.createOrReplaceUser(user)) {
+                logger.info("Create first user for system:");
+                logger.info("Username: " + user.getUsername());
+                logger.info("Password: " + firstUserPassword);
+            } else {
+                logger.error("Creating first user failed!");
+            }
 
-        //System.out.println("meliSearch.createOrReplaceUser(user) = " + meliSearch.createOrReplaceUser(user));
-
-        System.out.println("meliSearch.searchUserByUsername(\"Skillkiller\") = " + meliSearch.searchUserByUsername("Skillkiller"));
+        }
     }
 }
