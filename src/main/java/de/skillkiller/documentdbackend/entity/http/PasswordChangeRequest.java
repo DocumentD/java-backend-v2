5e63e623ab7848b99d12bded22c546496955d530
Sebastian Espei <seblsebastian@aol.de>
2020-09-28 03:38:13 +0200

    Change json representation

--- src/main/java/de/skillkiller/documentdbackend/entity/http/PasswordChangeRequest.java
+++ src/main/java/de/skillkiller/documentdbackend/entity/http/PasswordChangeRequest.java
@@ -1,9 +1,13 @@
 package de.skillkiller.documentdbackend.entity.http;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class PasswordChangeRequest {
+    @JsonProperty("oldpassword")
     private String oldPassword;
+
+    @JsonProperty("newpassword")
     private String newPassword;
 }
