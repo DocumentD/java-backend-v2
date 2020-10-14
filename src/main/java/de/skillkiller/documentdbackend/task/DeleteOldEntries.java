0457f083a8b5bbe3fdd6542f47c27ddc19ac1704
Sebastian Espei <seblsebastian@aol.de>
2020-10-14 22:58:10 +0200

    Add getAllData task

--- src/main/java/de/skillkiller/documentdbackend/task/DeleteOldEntries.java
+++ src/main/java/de/skillkiller/documentdbackend/task/DeleteOldEntries.java
@@ -53,6 +53,10 @@ public class DeleteOldEntries {
 
     @Scheduled(fixedRate = 24 * 60 * 60 * 1000, initialDelay = 1 * 1000)
     public void getAllData() {
+        HashMap<String, User> users = new LinkedHashMap<>();
+        List<Document> documents = new LinkedList<>();
+        List<File> checkedFiles = new ArrayList<>();
+
         databaseLockService.lockNewWriteOperations();
 
         logger.info("Wait for finishing currently running write operations");
@@ -80,9 +84,6 @@ public class DeleteOldEntries {
             logger.info("All meilisearch update operations completed");
 
             logger.debug("Start dumping data");
-            HashMap<String, User> users = new LinkedHashMap<>();
-            List<Document> documents = new LinkedList<>();
-            List<File> checkedFiles = new ArrayList<>();
 
             List<User> getUser;
             int offset = 0;
@@ -105,61 +106,125 @@ public class DeleteOldEntries {
 
             logger.info("Collected all data from Meilisearch");
 
-            //TODO Check User autocompletion
+        } catch (InterruptedException | TimeoutException e) {
+            logger.warn("Ran in timeout!", e);
+            databaseLockService.unlockNewWriteOperations();
+            return;
+        }
 
-            // Check all documents for file and user
-            for (Document document : documents) {
-                File file = fileUtil.getFile(document);
-                boolean needDelete = false;
-                if (file.exists()) {
-                    if (file.isFile()) {
-                        if (users.containsKey(document.getUserId())) {
-                            checkedFiles.add(file);
-                        } else {
-                            needDelete = true;
-                            logger.warn("Document" + document.getId() + " exists but user does not");
-                        }
+        List<Document> documentsToDelete = new ArrayList<>();
+        // Check all documents for file and user
+        for (Document document : documents) {
+            File file = fileUtil.getFile(document);
+            boolean needDelete = false;
+            if (file.exists()) {
+                if (file.isFile()) {
+                    if (users.containsKey(document.getUserId())) {
+                        checkedFiles.add(file);
                     } else {
                         needDelete = true;
-
-                        logger.warn("Document " + document.getId() + " linked file is not a file");
+                        logger.warn("Document" + document.getId() + " exists but user does not");
                     }
                 } else {
                     needDelete = true;
-                    logger.warn("Document " + document.getId() + " has no file on disk");
+                    logger.warn("Document " + document.getId() + " linked file is not a file");
                 }
-
-                if (needDelete) {
-                    //TODO Delete file! Problem: write lock is active
-                }
+            } else {
+                needDelete = true;
+                logger.warn("Document " + document.getId() + " has no file on disk");
             }
 
-            if (new File(fileUtil.getBASE_DIR()).isDirectory()) {
-                Path start = Paths.get(fileUtil.getBASE_DIR());
-                try (Stream<Path> stream = Files.walk(start, Integer.MAX_VALUE)) {
-                    List<File> collect = stream
-                            .map(Path::toFile)
-                            .filter(File::isFile)
-                            .filter(Predicate.not(checkedFiles::contains))
-                            .collect(Collectors.toList());
-
-                    collect.forEach(file -> {
-                        logger.error(file.getName() + " has no Database Entry!");
-                        if (file.delete()) {
-                            logger.warn("Delete file " + file.getAbsolutePath());
-                        }
-                    });
-
-                } catch (IOException e) {
-                    logger.error("IO Exception on scan documents files on disk", e);
-                }
+            if (needDelete) {
+                documentsToDelete.add(document);
+                documentSearch.deleteDocumentBypassWriteLock(document.getId());
             }
-
-        } catch (InterruptedException | TimeoutException e) {
-            logger.warn("Ran in timeout!", e);
-        } finally {
-            databaseLockService.unlockNewWriteOperations();
         }
+
+        documents.removeAll(documentsToDelete);
+        documentsToDelete.clear();
+
+        if (new File(fileUtil.getBASE_DIR()).isDirectory()) {
+            Path start = Paths.get(fileUtil.getBASE_DIR());
+            try (Stream<Path> stream = Files.walk(start, Integer.MAX_VALUE)) {
+                List<File> collect = stream
+                        .map(Path::toFile)
+                        .filter(File::isFile)
+                        .filter(Predicate.not(checkedFiles::contains))
+                        .collect(Collectors.toList());
+
+                collect.forEach(file -> {
+                    logger.error(file.getName() + " has no Database Entry!");
+                    if (file.delete()) {
+                        logger.warn("Delete file " + file.getAbsolutePath());
+                    }
+                });
+
+            } catch (IOException e) {
+                logger.error("IO Exception on scan documents files on disk", e);
+            }
+        }
+
+        HashMap<String, Set<String>> userCompanies = new HashMap<>();
+        HashMap<String, Set<String>> userCategories = new HashMap<>();
+
+        for (Document document : documents) {
+            if (document.getCompany() != null) {
+                Set<String> companies;
+                if (userCompanies.containsKey(document.getUserId())) {
+                    companies = userCompanies.get(document.getUserId());
+                } else {
+                    companies = new HashSet<>();
+                    userCompanies.put(document.getUserId(), companies);
+                }
+
+                companies.add(document.getCompany());
+            }
+
+            if (document.getCategory() != null) {
+                Set<String> categories;
+                if (userCategories.containsKey(document.getUserId())) {
+                    categories = userCategories.get(document.getUserId());
+                } else {
+                    categories = new HashSet<>();
+                    userCategories.put(document.getUserId(), categories);
+                }
+                categories.add(document.getCategory());
+            }
+        }
+
+        // Calculate user autocompletion
+        for (Map.Entry<String, User> userEntry : users.entrySet()) {
+            boolean updateUser = false;
+            User user = userEntry.getValue();
+            Set<String> calculatedCompanies = userCompanies.getOrDefault(user.getId(), new HashSet<>());
+
+            if (!user.getCompanies().equals(calculatedCompanies)) {
+                logger.warn("Find difference in user companies");
+                logger.debug("UserId: " + user.getId());
+                logger.debug("User categories: " + user.getCompanies().toString());
+                logger.debug("Calculated categories: " + calculatedCompanies.toString());
+                user.setCompanies(calculatedCompanies);
+                updateUser = true;
+            }
+
+            Set<String> calculatedCategories = userCategories.getOrDefault(user.getId(), new HashSet<>());
+
+            if (!user.getCategories().equals(calculatedCategories)) {
+                logger.warn("Find difference in user categories");
+                logger.debug("UserId: " + user.getId());
+                logger.debug("User categories: " + user.getCategories().toString());
+                logger.debug("Calculated categories: " + calculatedCategories.toString());
+                user.setCategories(calculatedCategories);
+                updateUser = true;
+            }
+
+            if (updateUser) {
+                userSearch.createOrReplaceUserBypassWriteLock(user);
+            }
+        }
+
+        databaseLockService.unlockNewWriteOperations();
+
     }
 
     // Rate = 6h
